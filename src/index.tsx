f2f97b13c1eda773ff6f21578b6ef983865a43d7
Shiva <Shivagautam2002@gmail.com>
2025-07-21 09:16:45 +0530

    chore: update entry point for improved project structure

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration.ts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+); 
 root.render(
   <React.StrictMode>
     <App />
